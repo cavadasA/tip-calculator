65994ec709ba8f09683e0d97b741a7bd59974a8c
Antonio Cavadas <antonio.cavadas.gu@gmail.com>
2021-07-24 01:49:57 +0200

    alpha version done, pending lg design

--- src/component/button.js
+++ src/component/button.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 export function TipButton(props) {
     return (
-        <button type="button" class="btn btn-lg btn-block text-white my-2" style={{backgroundColor: "#00474b", fontSize: 2 + "em"}}>{props.percentage}%</button>
+        <button type="button" className="btn btn-lg btn-block text-white my-2" style={props.selected === true ? {backgroundColor: "#26c2ad", fontSize: 2 + "em"} : {backgroundColor: "#00474b", fontSize: 2 + "em"}}>{props.percentage}%</button>
     )
 }
 
 TipButton.propTypes = {
-    percentage: PropTypes.number
+    percentage: PropTypes.number,
+    selected: PropTypes.bool
 }
